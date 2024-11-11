dcca160fa4d772d22fc5c963bd6eab13b3baa13c
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-11-11 15:24:16 -0300

    Users usuario, adm, empresa

---
+++ Users/Users.sql
@@ -0,0 +1,5 @@
+CREATE USER IF NOT EXISTS 'usuario'@'localhost' IDENTIFIED BY 'user123';
+
+CREATE USER IF NOT EXISTS 'administrador'@'localhost' IDENTIFIED BY 'adm123';
+
+CREATE USER IF NOT EXISTS 'empresa'@'localhost' IDENTIFIED BY 'empresa123';
