5c1cb75fba53d737bef98389557f389f9efa498c
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-11-11 17:31:51 -0300

    fazendo segunda view, usuarioHistorico

---
+++ Views.sql
@@ -0,0 +1,16 @@
+USE drive;
+
+CREATE VIEW usuarioArquivo AS 
+SELECT 
+nome AS nome_arquivo,
+tipo AS tipo_arquivo,
+URL,
+Permissoes_acesso,
+localizacao,
+tamanho,
+data_modificacao
+FROM drive.arquivo WHERE id_usuario = 4;
+
+DROP VIEW usuarioArquivo;
+
+
--- Views/Views.sql
+++ Views/Views.sql
@@ -1,7 +1,9 @@
 USE drive;
 
+-- view para usuario ver seus arquivos
 CREATE VIEW usuarioArquivo AS 
-SELECT nome AS nome_arquivo,
+SELECT 
+nome AS nome_arquivo,
 tipo AS tipo_arquivo,
 URL,
 Permissoes_acesso,
@@ -10,6 +12,19 @@ tamanho,
 data_modificacao
 FROM drive.arquivo WHERE id_usuario = 4;
 
+-- view para usuario ver o historico dele
+CREATE VIEW usuarioHistorico AS
+SELECT
+operacao,
+data_operacao,
+hora,
+conteudo_alterado,
+id_usuario,
+id_arquivo
+FROM drive.historico WHERE id_usuario = 4;
 
+
+
+DROP VIEW usuarioArquivo;
 
 
