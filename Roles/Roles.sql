7a575136239daa37e5406e83a0756ddde9c44821
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-11-15 20:23:59 -0300

    Papel empresa e Papel administrador feitos!

--- Roles/Roles.sql
+++ Roles/Roles.sql
@@ -8,3 +8,22 @@ FLUSH PRIVILEGES;
 SHOW GRANTS FOR 'papelusuario';
 DROP ROLE 'papelusuario';
 
+-- criando o papel empresa
+CREATE ROLE 'papelempresa';
+GRANT SELECT ON empresaUsuarios TO 'papelempresa';
+GRANT SELECT ON empresaArquivos TO 'papelempresa';
+GRANT 'papelempresa' TO 'empresa'@'localhost';
+FLUSH PRIVILEGES;
+
+-- criando o papel administrador
+CREATE ROLE 'papeladministrador';
+GRANT SELECT, INSERT, UPDATE, DELETE ON drive.* TO 'papeladministrador';
+GRANT 'papeladministrador' TO 'administrador'@'localhost';
+FLUSH PRIVILEGES;
+
+
+
+SHOW GRANTS FOR 'papelempresa';
+SHOW GRANTS FOR 'empresa'@'localhost';
+DROP ROLE 'papelempresa';
+DROP USER 'administrador'@'localhost';
