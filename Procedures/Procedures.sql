bacc31c7b3eac61deb260e9555277b38becf2aab
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-11-16 16:54:55 -0300

    Procedures Verificar_atividades + Chavear + Remover_acessos feito

--- Procedures/Procedures.sql
+++ Procedures/Procedures.sql
@@ -1,3 +1,15 @@
+-- Procedure para atualizar a ultima_versao para a data atual
+DELIMITER $$
+CREATE PROCEDURE Verificar_atividades()
+BEGIN
+	UPDATE atividades_recentes SET ultima_versao = CURDATE();
+END $$
+DELIMITER ;
+
+SET SQL_SAFE_UPDATES = 0; -- tive que desligar a segurança para conseguir atualizar a coluna da table toda
+CALL Verificar_atividades();
+SET SQL_SAFE_UPDATES = 1;
+DROP PROCEDURE Verificar_atividades;
 
 -- Procedure para contar quantos usuarios tem um mesmo arquivo
 DELIMITER $$
@@ -5,10 +17,43 @@ CREATE PROCEDURE Conta_usuarios (IN id_arq INT)
 BEGIN
 	DECLARE cont INT;
     
-    SELECT COUNT(DISTINCT id_usuario) INTO cont FROM drive.historico WHERE id_arquivo = id_arq;
-    
+    SELECT COUNT(DISTINCT id_usercompartilhado) INTO cont FROM drive.compartilhamento WHERE id_arquivo = id_arq;
+    SET cont := cont+1;
     SELECT cont AS qtd_usuarios;
 END $$
 DELIMITER ;
 
 CALL Conta_usuarios(1);
+DROP PROCEDURE Conta_usuarios;
+
+-- Procedure para Atualizar o arquivo de prioritario para não prioritario e vice versa;
+DELIMITER $$
+CREATE PROCEDURE Chavear(IN id_arq INT)
+BEGIN
+		IF (SELECT atividades_recentes.acesso FROM atividades_recentes WHERE id_arquivo = id_arq) = 'prioritário' THEN
+			UPDATE atividades_recentes SET acesso = 'não prioritário' WHERE id_arquivo = id_arq;
+		ELSE
+			UPDATE atividades_recentes SET acesso = 'prioritário' WHERE id_arquivo = id_arq;
+		END IF;
+END $$
+DELIMITER ;
+
+CALL Chavear(3);
+DROP PROCEDURE Chavear;
+
+-- Procedure para Remover_acesso
+DELIMITER $$
+CREATE PROCEDURE Remover_acessos(IN id_arq INT)
+BEGIN
+	DELETE FROM compartilhamento WHERE id_arquivo = id_arq;
+END $$
+DELIMITER ;
+
+CALL Remover_acessos(10);
+
+
+
+
+
+
+
